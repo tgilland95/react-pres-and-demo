6fda6989198bf88579a74feec825d33ed3e01789
tgilland95 <tylergilland@gmail.com>
2018-04-01 20:53:43 -0600

    added react-native dir to node script

--- presentation/updateFileNames.js
+++ presentation/updateFileNames.js
@@ -1,9 +1,14 @@
-var fs = require("fs")
-var sites = "./assets/sites_using_react/"
+const fs = require("fs")
+const sites_using_react = "./assets/sites_using_react/"
+const sites_using_react_native = "./assets/sites_using_react_native/"
 let res = []
-const filenames = () => fs.readdirSync(sites, (err, items) => ({ names: items }))
+const reactNames = () => fs.readdirSync(sites_using_react, (err, items) => ({ names: items }))
+const reactNativeNames = () => fs.readdirSync(sites_using_react_native, (err, items) => ({ names: items }))
 fs.writeFile(
     "./assetFileNames.js",
-    `export const names = ${JSON.stringify(filenames())}`,
+    `export const names = {
+        react: ${JSON.stringify(reactNames())},
+        reactNative: ${JSON.stringify(reactNativeNames())},
+ }`,
     (err) => (err ? console.log(err) : console.log("\n***** updated file names successfuly\n"))
 )
