caa699c02d2751dcc18909fdfe8cfd4c5db1cae5
Joshua-Jenkins33 <joshuabyu@yahoo.com>
2018-04-02 17:24:14 -0600

    added trends picture and slide

---
+++ presentation/presentation/slides/Trends.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Heading, Text, Quote, List, ListItem, BlockQuote, Cite, Image, tile, Slide } from "spectacle"
+import { names } from "../../assetFileNames"
+import glamorous from "glamorous"
+
+export const BrandTile = glamorous.img({
+    display: "inline-flex",
+    height: "128px",
+    width: "128px",
+    margin: "10px",
+})
+
+export const Trends = () => (
+    <Slide>
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            react, angular, vue download trends
+        </Heading>
+
+
+            <Image src={require(`../../assets/other/${"trends.png"}`)} />
+    </Slide>
+)
